2b64407b699901429fe8bd08caadebda6576e39e
Shruthick <110411394+shruthick99@users.noreply.github.com>
2024-11-06 23:59:00 -0500

    Update and rename hello.java to HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hello {
+public class HelloController {  // Class name should match the file name
 
     @GetMapping("/hello")
     public String sayHello() {
