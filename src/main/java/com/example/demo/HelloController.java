d49f12c901d4e6286994bebc5ade0c7fdbda4df8
Shruthick <110411394+shruthick99@users.noreply.github.com>
2024-11-12 15:21:31 -0500

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {  // Class name should match the file name
     // Map to root URL ("/") instead of "/hello"
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, Miracle!";
+        return "Hello, Miracle 12346677!";
     }
 }
